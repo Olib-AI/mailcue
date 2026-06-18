28b424ac4b3495bfdb5ce7cd72548af09d5d877a
Akram Hasan Sharkar <ahs@akhaura.info>
2026-06-18 12:31:24 +0100

    Relax React compiler lint diagnostics

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,23 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
+const reactCompilerDiagnosticRules = [
+  "react-hooks/config",
+  "react-hooks/error-boundaries",
+  "react-hooks/gating",
+  "react-hooks/globals",
+  "react-hooks/immutability",
+  "react-hooks/incompatible-library",
+  "react-hooks/preserve-manual-memoization",
+  "react-hooks/purity",
+  "react-hooks/refs",
+  "react-hooks/set-state-in-effect",
+  "react-hooks/set-state-in-render",
+  "react-hooks/static-components",
+  "react-hooks/unsupported-syntax",
+  "react-hooks/use-memo",
+];
+
 export default tseslint.config(
   { ignores: ["dist"] },
   {
@@ -19,6 +36,9 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...Object.fromEntries(
+        reactCompilerDiagnosticRules.map((rule) => [rule, "warn"])
+      ),
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
